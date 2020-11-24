02a9391b3f59f55f71021a23907eba6f14a6a243
fumie-03 <cocoon0403.f@gmail.com>
2020-11-24 23:43:45 +0900

    ActiveHash導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,11 +1,15 @@
 class Item < ApplicationRecord
   has_one :buyer
   belongs_to :user
-  # belongs_to_active_hash :category
-  # belongs_to_active_hash :condition
-  # belongs_to_active_hash :postage
-  # belongs_to_active_hash :prefecture
-  # belongs_to_active_hash :send_time
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :category
+  belongs_to_active_hash :condition
+  belongs_to_active_hash :postage
+  belongs_to_active_hash :prefecture
+  belongs_to_active_hash :send_time
+
+
   has_one_attached :image
 
 end
--- db/migrate/20201124070053_create_items.rb
+++ db/migrate/20201124070053_create_items.rb
@@ -1,15 +1,15 @@
 class CreateItems < ActiveRecord::Migration[6.0]
   def change
     create_table :items do |t|
-      t.string :name
-      t.text :explanation
+      t.string :name           , null: false                
+      t.text :explanation      , null: false
       t.references :user, foreign_key: true
-      t.integer :category_id
-      t.integer :condition_id
-      t.integer :postage_id
-      t.integer :prefecture_id
-      t.integer :send_time_id
-      t.integer :price
+      t.integer :category_id   , null: false
+      t.integer :condition_id  , null: false
+      t.integer :postage_id    , null: false
+      t.integer :prefecture_id , null: false
+      t.integer :send_time_id  , null: false
+      t.integer :price         , null: false
       t.timestamps
     end
   end
