c02fd088b4672d802f7ca2857f284b3016205d5f
fumie-03 <cocoon0403.f@gmail.com>
2020-11-25 15:25:39 +0900

    商品情報のテーブルへの保存

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -9,11 +9,16 @@ class ItemsController < ApplicationController
   end
 
   def create
-    Item.create(item_params)
+    @item = Item.create(item_params)
+    unless @item.valid?
+      render :new and return
+    end
+    @item.save
+    redirect_to root_path
   end
 
   private
   def item_params
-    params.require(:item).permit(:nmae, :explanation, :user_id, :condition_id, :postage_id, :prefecture_id, :send_time_id, :price, :image).merge(user_id: current_user.id)
+    params.require(:item).permit(:name, :explanation, :user_id, :category_id, :condition_id, :postage_id, :prefecture_id, :send_time_id, :price, :image).merge(user_id: current_user.id)
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -26,7 +26,7 @@ class Item < ApplicationRecord
     validates :condition_id
     validates :postage_id
     validates :prefecture_id
-    validates :stand_time_id
+    validates :send_time_id
   end
 
   validates :price, format: { with: /\A[0-9]+\z/, message: "is invalid. Input half-width numbers."}, numericality: {greater_than_or_equal_to: 300, less_than: 9999999}
