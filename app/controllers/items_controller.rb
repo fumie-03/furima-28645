716efb31c846ca0aa39a07f6ad66a46d7c057fe1
fumie-03 <cocoon0403.f@gmail.com>
2020-12-01 17:06:13 +0900

    商品詳細の表示

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -18,6 +18,10 @@ class ItemsController < ApplicationController
     redirect_to root_path
   end
 
+  def show
+    @item = Item.find(params[:id])
+  end
+
   private
   def item_params
     params.require(:item).permit(:name, :explanation, :user_id, :category_id, :condition_id, :postage_id, :prefecture_id, :send_time_id, :price, :image).merge(user_id: current_user.id)
