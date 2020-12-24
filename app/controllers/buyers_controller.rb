f11d81755b980a02e01fcbe78390a58507c8613c
fumie-03 <cocoon0403.f@gmail.com>
2020-12-24 12:56:13 +0900

    soldout機能実装

--- app/controllers/buyers_controller.rb
+++ app/controllers/buyers_controller.rb
@@ -1,4 +1,5 @@
 class BuyersController < ApplicationController
+  before_action :sold_item, only:[:index, :create]
 
   def index
     @contact = ContactForm.new
@@ -26,9 +27,17 @@ class BuyersController < ApplicationController
     )
   end
 
-  
+  private
+
   def contact_params
     params.require(:contact_form).permit(:user_id, :item_id, :post_code, :prefecture_id, :city, :street_number, :building_name, :phone_number,).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
+  end
+
+  def sold_item
+    @item = Item.find(params[:item_id])
+    if @item.buyer.present?
+      redirect_to root_path
+    end
   end
 end
 
